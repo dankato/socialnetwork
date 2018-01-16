980454a543ea9e1b50f352ddebe9433fae589b65
dankato <apearmor@gmail.com>
2018-01-15 23:00:46 -0800

    added more validation form checks

--- register.php
+++ register.php
@@ -47,6 +47,22 @@
           } else {
             echo "Emails do not match.";
           }
+          if(strlen($firstname) > 25 || strlen($firstname) < 2) {
+            echo "Your first name must be between 2 and 25 characters.";
+          }
+          if(strlen($lastname) > 25 || strlen($lastname) < 2) {
+            echo "Your last name must be between 2 and 25 characters.";
+          }
+          if($password != $password2) {
+            echo "Your passwords do not match.";
+          } else {
+            if(preg_match('/[^A-Za-z0-9]/', $password)) {
+              echo "Your password can only contain characters or numbers.";
+            }
+          }
+          if(strlen($password > 30 || strlen($password) < 5)) {
+            echo "Your password must be between 5 and 30 characters.";
+          }
       }
     }
   }
