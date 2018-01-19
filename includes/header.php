b083fc710664482ee89ddd9dcb25949852a0815c
dankato <apearmor@gmail.com>
2018-01-19 00:02:03 -0800

    added nav icons

--- includes/header.php
+++ includes/header.php
@@ -4,6 +4,8 @@
   // prevent users to access site without logging in first
   if (isset($_SESSION['username'])) {
     $userLoggedIn = $_SESSION['username'];
+    $user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$userLoggedIn'");
+    $user = mysqli_fetch_array($user_details_query);
   } else {
     header("Location: register.php");
   }
@@ -17,6 +19,7 @@
     <!-- css -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/style.css">
+    <script src="https://use.fontawesome.com/8f99050c9f.js"></script>
     <!-- jquery/js -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
@@ -27,4 +30,14 @@
       <div class="logo">
         <a href="index.php">Social Network</a>
       </div>
+      <nav>
+        <a href="#"><?php echo $user['first_name']?></a>
+        <a href="index.php"><i class="fa fa-home" aria-hidden="true"></i></a>
+        <a href="#"><i class="fa fa-comment-o" aria-hidden="true"></i></a>
+
+        <a href="#"><i class="fa fa-exclamation" aria-hidden="true"></i></a>
+        <a href="#"><i class="fa fa-users" aria-hidden="true"></i></a>
+        <a href="#"><i class="fa fa-cog" aria-hidden="true"></i></a>
+        <a href="#"><i class="fa fa-sign-out" aria-hidden="true"></i></a>
+      </nav>
     </div>
