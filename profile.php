d73b8a5cf197ba6fe3f3e7c6d059ebf77b74c4ff
dankato <apearmor@gmail.com>
2018-01-24 13:09:02 -0800

    added styling to profile page

--- profile.php
+++ profile.php
@@ -1,9 +1,30 @@
 <?php
 include("includes/header.php");
+if(isset($_GET['profile_username'])) {
+  $username = $_GET['profile_username'];
+  $user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$username'");
+  $user_array = mysqli_fetch_array($user_details_query);
+  $num_friends = (substr_count($user_array['friend_array'], ",")) - 1;
+}
 ?>
+  <style type="text/css">
+    .wrapper {
+      margin-left: 0;
+      padding-left: 0;
+    }
+  </style>
 
+  <div class="profile_left">
+    <img src="<?php echo $user_array['profile_pic']; ?>" alt="">
+
+    <div class="profile_info">
+      <p><?php echo "Posts: " . $user_array['num_posts']; ?></p>
+      <p><?php echo "Likes: " . $user_array['num_likes']; ?></p>
+      <p><?php echo "Friends: " . $num_friends; ?></p>
+    </div>
+  </div>
   <div class="main_column column">
-    this is the profile page
+    <?php echo $username; ?>
   </div>
   </div>
 </body>
