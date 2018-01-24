380abed25feca332b359666025a5d51469f2cc60
dankato <apearmor@gmail.com>
2018-01-24 00:09:01 -0800

    like unlike functional

--- like.php
+++ like.php
@@ -36,9 +36,20 @@
       // Like button
       if(isset($_POST['like_button'])) {
         $total_likes++;
-        $query = mysqli_query($con, "UPDATE posts SET likes='$total_likes' WHERE post_id='$post_id'");
+        $query = mysqli_query($con, "UPDATE posts SET likes='$total_likes' WHERE id='$post_id'");
         $total_user_likes++;
         $user_likes = mysqli_query($con, "UPDATE users SET num_likes='$total_user_likes' WHERE username='$user_liked'");
+        $insert_user = mysqli_query($con, "INSERT INTO likes VALUES('', '$userLoggedIn', '$post_id')");
+        // insert notifs here
+      }
+
+      // Unlike button
+      if(isset($_POST['unlike_button'])) {
+        $total_likes--;
+        $query = mysqli_query($con, "UPDATE posts SET likes='$total_likes' WHERE id='$post_id'");
+        $total_user_likes--;
+        $user_likes = mysqli_query($con, "UPDATE users SET num_likes='$total_user_likes' WHERE username='$user_liked'");
+        $insert_user = mysqli_query($con, "DELETE FROM likes WHERE username='$userLoggedIn' AND post_id='$post_id'");
       }
 
       // check for previous likes
