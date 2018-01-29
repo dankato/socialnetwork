b15439bc69e0df5fe1b910627de520ed9ae96eba
dankato <apearmor@gmail.com>
2018-01-28 21:47:35 -0800

    profile pic upload

--- includes/header.php
+++ includes/header.php
@@ -22,11 +22,14 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/style.css">
     <script src="https://use.fontawesome.com/8f99050c9f.js"></script>
+    <link rel="stylesheet" href="assets/css/jquery.Jcrop.css" type="text/css" />
     <!-- jquery/js -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/4.4.0/bootbox.min.js"></script>
     <script type="text/javascript" src="assets/js/main.js"></script>
+    <script src="assets/js/jquery_jcrop.js"></script>
+    <script src="assets/js/jcrop_bits.js"></script>
   </head>
   <body>
 
